c284e359ae60fb2d3c6b9bd030ce41f51b22d2c1
Arthur <arthurhenrique0708@gmail.com>
2023-09-12 15:07:35 -0300

    .

--- scripts-CONTADOR.sql
+++ scripts-CONTADOR.sql
@@ -26,9 +26,9 @@ insert into TB_salas
 	('Sala 25', 'Informática - Montagem, Configuração e Redes', '2º', 0),
 	('Sala 26', 'Informática - Programação', '2º', 0),
     
-	('AUDITORIO', 'CATE e Simulação de Entrevista', '3º', 0),
-	('AUDITORIO', 'SAS', '3º', 0),
-	('Sala 33', 'LINKEDIM', '3º', 0);
+	('AUDITÓRIO', 'CATE e Simulação de Entrevista', '3º', 0),
+	('AUDITÓRIO', 'SAS', '3º', 0),
+	('Sala 33', 'LINKEDIN', '3º', 0);
     
     
     -- aumentar contador
