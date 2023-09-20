9bc3dbef1addf600de8c67fe2545ee4ddc1d4113
BRUNO DE OLIVEIRA <brunodeoliveira.22.10@gmail.com>
2023-09-20 00:12:12 -0300

    Update connections.js

--- src/repository/connections.js
+++ src/repository/connections.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const conexao = await mysql.createConnection({
-    host: process.env.HOST,
-    database: process.env.DB,
-    user: process.env.USER,
-    password: process.env.PWD
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DB,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD
 });
 
 console.log('Banco de dados conectado com sucesso');
