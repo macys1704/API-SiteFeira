213b1d4024fa675fbc0de3eae3b59b454477a389
igor-nasciment0 <141491741+igor-nasciment0@users.noreply.github.com>
2023-09-27 16:07:47 -0300

    corrigindo erros

--- src/repository/loginadmRepositoy.js
+++ src/repository/loginadmRepositoy.js
@@ -1,15 +1,10 @@
 import conexao from "./connections.js";
 
 export async function verificarLogin() {
-  try {
     const comando = `
       SELECT nm_login AS login, ds_senha AS senha
       FROM tb_login_adm
     `;
     const [dados] = await conexao.query(comando);
     return dados[0];
-  } catch (error) {
-    console.error('Erro ao buscar as credenciais:', error);
-    throw error;
-  }
 }
